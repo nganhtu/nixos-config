3e3f8063f13d36b07926752b6b6447d53e84ee1c
Rin <ribisachi0319@gmail.com>
2026-07-04 10:53:50 +0700

    chore: gỡ figma-linux

--- hosts/niquesse/configuration.nix
+++ hosts/niquesse/configuration.nix
@@ -179,7 +179,7 @@ in
 
     # App GUI (Giai đoạn 5b)
     spotify discord libreoffice-fresh pavucontrol
-    ristretto postman parsec-bin vscode figma-linux
+    ristretto postman parsec-bin vscode
 
     # Dev / LSP / formatters (Giai đoạn 5c) — helix tự nhận qua PATH
     nodejs typescript
